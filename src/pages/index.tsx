85399ddcdb6584600a70c18302e1d1ca49e73a97
andy <andersgee@gmail.com>
2022-12-25 19:18:57 +0100

    SEO and image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { GetStaticProps, NextPage } from "next";
 import { useRouter } from "next/router";
 import { useEffect, useId, useState } from "react";
 import { Chart } from "../components/Chart";
+import { SEO } from "../components/SEO";
 import { getSweCovidData, type CovidData } from "../utils/covid";
 
 type Props = {
@@ -30,27 +31,35 @@ const Page: NextPage<Props> = ({ covidData }) => {
   };
 
   return (
-    <div className="container flex flex-col items-center px-2">
-      <h1 className="my-10">Covid stats - How many had it?</h1>
-      <div className="flex items-baseline">
-        <label htmlFor={selectId} className="mr-1">
-          Region
-        </label>
-        <select
-          id={selectId}
-          value={selectedRegion}
-          onChange={(e) => handleChange(e.target.value)}
-          className="bg-white p-2 text-black dark:bg-black dark:text-white"
-        >
-          {Object.keys(covidData).map((region) => (
-            <option key={region} value={region}>
-              {region}
-            </option>
-          ))}
-        </select>
+    <>
+      <SEO
+        url="/"
+        title="covid stats"
+        description="Simple overview of covid cases in Sweden"
+        image="/images/share.png"
+      />
+      <div className="container flex flex-col items-center px-2">
+        <h1 className="my-10">Covid stats - How many had it?</h1>
+        <div className="flex items-baseline">
+          <label htmlFor={selectId} className="mr-1">
+            Region
+          </label>
+          <select
+            id={selectId}
+            value={selectedRegion}
+            onChange={(e) => handleChange(e.target.value)}
+            className="bg-white p-2 text-black dark:bg-black dark:text-white"
+          >
+            {Object.keys(covidData).map((region) => (
+              <option key={region} value={region}>
+                {region}
+              </option>
+            ))}
+          </select>
+        </div>
+        <Chart data={covidData[selectedRegion]} />
       </div>
-      <Chart data={covidData[selectedRegion]} />
-    </div>
+    </>
   );
 };
 
